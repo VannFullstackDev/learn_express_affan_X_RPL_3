0e313d070615cc13fe5815c5956674e44d266a04
VannFullstackDev <vann.developer2@gmail.com>
2024-03-21 02:45:45 +0700

    first commit

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express() //membuat variabel yang berisikan semua function di dalam 
 const port = 3000 // membuat sebuah port untuk di jalankan ke web
 
 app.get('/vann',(req,res) => { // app.get adalah sebuah fungsi/metode untuk membuat routing sederhana dan di beri sebuah parameter req dan res
-    console.log('woyyy') // ini adalah cara untuk mengirim code ke dalam console 
+    console.log('woyyy') // ini adalah cara untuk mengirim code ke dalam console
     res.send('hallo ini web afan') //res.send di gunakan untuk menampilkan sebuah code ke dalam halaman pada web
 })
 
 app.listen(port, () => { //listen.port digunakan untuk memerintahkan code ke dalam port yg kita berikan
-    console.log('nyambung nyet')
+    console.log('nyambung nyet')// ini digunakan untuk memeriksa apakah sudah berhasil menghubungkan port tersebut
 })
